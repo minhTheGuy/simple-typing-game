55da161342314cbf35b1807a867b939467aebd43
Dang Minh <52200277@student.tdtu.edu.vn>
2025-06-04 14:02:13 +0700

    Create backend project

---
+++ backend/src/main/java/com/minh/simple_typing_game/SimpleTypingGameApplication.java
@@ -0,0 +1,13 @@
+package com.minh.simple_typing_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTypingGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTypingGameApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/minh/simple_typing_game/SimpleTypingGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minh.simple_typing_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTypingGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
